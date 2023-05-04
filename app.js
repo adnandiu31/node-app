69627e2b60021386cd24fb8259233942c0660add
adnandiu31 <adnandiu31@gmail.com>
2023-05-05 00:22:56 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello Worlsdfadfd!  this a code');
+});
+app.get('/foo', function (req, res) {
+    res.send('Hello Worldsfsodfhkshdfkjkd!');
+  });
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
